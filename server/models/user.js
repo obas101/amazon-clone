f29a1478ffbb5a9ca19cfdbe6a494f266801b18d
Yunusa Onivehu <obadobay@gmail.com>
2020-06-21 16:07:21 +0100

    first commit

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email: {type: String, unique: true, required: true},
+    password: {type: Schema.Types.ObjectId, ref: "Address"}
+});
+
+module.exports = mongoose.model("User", UserSchema);
