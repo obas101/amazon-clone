7de424286f1e554b485b02a7c797bf554a53e80a
Yunusa Onivehu <obadobay@gmail.com>
2020-08-01 09:02:13 +0100

    added navbar

---
+++ client/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="nav-opt-sprite nav-locate-us nav-lang-en nav-ssl nav-unrec">
+      <div class="container-fluid dekstop-nav">
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="logo-area">
+              <a href="#">
+                <img src="/img/logo.png" alt="Logo" class="img-fluid" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -22,7 +22,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/font-awesome/css/all.css' },
+      { rel: 'stylesheet', href: '/css/default.css' }
     ]
   },
   /*
